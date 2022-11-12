92cbba56c436d902299c7f3bc35c358eb9d6535a
Lapor Chen <31620109+laporchen@users.noreply.github.com>
2022-11-12 12:43:36 +0800

    fix: correctly change the base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), WindiCSS() ]
+  plugins: [vue(), WindiCSS() ],
+  base: "/GPAv2"
 })
