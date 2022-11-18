e47516989e161cacaeba728f6bcfab7bd1cf50a2
Lapor Chen <31620109+laporchen@users.noreply.github.com>
2022-11-18 23:00:13 +0800

    fix: update axios settings

--- src/axios.ts
+++ src/axios.ts
@@ -1,4 +1,4 @@
 import axios from 'axios'
 
 axios.defaults.baseURL = "https://ntnufetch.lapor-chen.com/"
-axios.defaults.headers.post['Access-Control-Allow-Origin'] = "*"
+//axios.defaults.headers.post['Access-Control-Allow-Origin'] = "*"
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -90,7 +90,6 @@ const rules = {
 		trigger: "blur"
 	}
 }
-const result = ref<Grade[]>([])
 const scores = ref<Map<string,Map<string,Grade[]>>>(new Map())
 const getResult = ref(false)
 const handleValidateClick = async (e: MouseEvent) => {
