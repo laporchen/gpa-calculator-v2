696b892284e149f8b4c029ba26465f3659e549b6
Lapor Chen <31620109+laporchen@users.noreply.github.com>
2022-11-19 23:45:34 +0800

    fix: set default tab

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -2,7 +2,7 @@
 	<n-layout-content>
 		<div class="justify-center pt-4 mx-auto md:w-2/3 h-full max-w-11/12 lg:w-screen-md">
 			<n-card class="min-h-11/12">
-				<n-tabs type="line" animated default-value="show">
+				<n-tabs type="line" animated default-value="manual">
 					<n-tab-pane display-directive="show" name="manual" tab="手動輸入">
 						<ManualInput />
 					</n-tab-pane>
